c6072685d3c04a2d2775bb991c97375544332ef5
Gokila23 <gokilagowtham10@gmail.com>
2016-09-08 12:07:41 +0530

    Add files via upload

---
+++ power.java
@@ -0,0 +1,14 @@
+package Logic;
+
+import java.util.Scanner;
+
+public class power {
+public static void main(String[] args) {
+	Scanner s=new Scanner(System.in);
+	double n=s.nextInt();
+	
+	double ss=Math.pow(n, 3);
+	System.out.println(ss);
+	
+}
+}
